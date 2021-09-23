82be0fccf52ef16bea440ba950e905fb8d81d4b2
Greg <greg_markarian@hotmail.com>
2021-09-23 17:32:07 +0300

    high speed text button

---
+++ src/components/InternetProvider.js
@@ -0,0 +1,28 @@
+import React from "react";
+import cn from "classnames";
+
+function InternetProvider() {
+  return (
+    <div>
+      <div className={cn("camera")}>
+        <div className={cn("container mx-auto")}>
+          <div className={cn("grid grid-cols-2")}>
+            <div className={cn("col-span-1")}>
+               
+                <p className = {cn("text-white font-bold text-lg")}>Your Local High Speed internet Provider</p>
+                <button className = {cn("bg-blue-700 p-2 rounded text-sm")}>Check Availability</button>
+                
+                <button className = {cn("text-white text-sm pl-4 font-bold")}>Switch is easy</button>
+                <div className = {cn("pt-6")}></div>
+                <div className = {cn("border-1")}></div>
+                <p className = {cn("pt-4 text-sm text-white")}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tempore porro in libero voluptatem iste officia id nulla laboriosam molestias. A corporis totam ex cumque rerum deserunt, accusantium repudiandae unde.</p>
+               
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InternetProvider;
