a0e17e5d2edcaa31b1e6cc5fa8316167281f53ba
Greg <greg_markarian@hotmail.com>
2021-09-23 12:57:37 +0300

    ADD: component internet selection

---
+++ src/components/IntSelection.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import cn from 'classnames'
+
+function IntSelection(props) {
+    return (
+        <div>
+            <div className = {cn("w-3/12")}>
+                <div>
+                    <p className = {cn(" text-sm text-blue-800 font-bold bg-gray-300 p-3 ")}>01. <span className = {cn(" text-sm pl-3 text-blue-800 font-bold")}>WIFI INTERNET</span> </p>
+                    <div className = {cn("pt-1")}></div>
+                    <p className = {cn(" text-sm text-blue-800 font-bold bg-gray-300 p-3")}>03. <span className = {cn(" text-sm pl-3 text-blue-800 font-bold")}>TV BOX</span> </p>
+                    <div className = {cn("pt-1")}></div>
+                    <p className = {cn(" text-sm text-blue-800 font-bold bg-gray-300 p-3")}>04. <span className = {cn(" text-sm pl-3 text-blue-800 font-bold")}>SMART HOME</span> </p>
+                    <div className = {cn("pt-1")}></div>
+                    <p className = {cn(" text-sm text-blue-800 font-bold bg-gray-300 p-3")}>05. <span className = {cn(" text-sm pl-3 text-blue-800 font-bold")}>SATELLITE TV</span> </p>
+                    <div className = {cn("pt-1")}></div>
+                    <p className = {cn(" text-sm text-blue-800 font-bold bg-gray-300 p-3")}>06. <span className = {cn(" text-sm pl-3 text-blue-800 font-bold")}>BROADBAND</span> </p>
+                    <div className = {cn("pt-1")}></div>
+                    <p className = {cn(" text-sm text-blue-800 font-bold bg-gray-300 p-3")}>07. <span className = {cn(" text-sm pl-3 text-blue-800 font-bold")}>INTERNET</span> </p>
+                    <div className = {cn("pt-1")}></div>
+                    <p className = {cn(" text-sm text-blue-800 font-bold bg-gray-300 p-3")}>08. <span className = {cn(" text-sm pl-3 text-blue-800 font-bold")}>BUSINESS</span> </p>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default IntSelection
