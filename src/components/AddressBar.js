48de2fdd504cd6a6df72c7de28c4bc1d14441f39
Greg <greg_markarian@hotmail.com>
2021-09-24 13:32:12 +0300

    ADD: text description Adress bar

--- src/components/AddressBar.js
+++ src/components/AddressBar.js
@@ -3,51 +3,52 @@ import cn from "classnames";
 
 function AddressBar() {
   return (
-    <div className={cn("pt-40")}>
-      <div className={cn("fire")}>
+    <div className={cn("pt-16")}>
+      <div className={cn("firebar")}>
         <div className={cn("container mx-auto")}>
-        
-          <p className={cn("text-white")}>
-            Enter your address to find deals in your area:
-          </p>
-          <div class="flex">
-            <div class="flex-grow pl-2">
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Street address"
-                className={cn("p-2")}
-              />
-            </div>
-            <div class="flex-shrink pl-1">
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Apt/Unite"
-                className={cn("p-2")}
-              />
-            </div>
-            <div class="flex-shrink pl-1">
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Enter zipcoide"
-                className={cn("p-2")}
-              />
-            </div>
-            <div class="flex-grow">
-              <button className={cn("bg-blue-200 text-sm ")}>
-                Check availability
-              </button>
+          <div className={cn("pt-4")}>
+            <p className={cn("text-white")}>
+              Enter your address to find deals in your area:
+            </p>
+            <div className="flex">
+              <div className="flex-grow pl-2">
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Street address"
+                  className={cn("p-2")}
+                />
+              </div>
+              <div className="flex-shrink pl-1">
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Apt/Unite"
+                  className={cn("p-2")}
+                />
+              </div>
+              <div className="flex-shrink pl-1">
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Enter zipcoide"
+                  className={cn("p-2")}
+                />
+              </div>
+              <div className="flex-grow pl-1">
+                <div className = {cn("flex justify-center ritems-center")}>
+                <button className = {cn("bg-blue-600 text-sm px-18")}>Check Availability</button>
+                </div>
+              </div>
+             
             </div>
           </div>
         </div>
-        </div>
       </div>
-    
+    </div>
   );
 }
 
