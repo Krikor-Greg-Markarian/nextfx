a1c9d20e0b32ae5534080d02d790dca847b5476a
Greg <greg_markarian@hotmail.com>
2021-09-24 10:36:20 +0300

    ADD: components together

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,8 @@ import InternetProvider from "../src/components/InternetProvider";
 import Unlimited from "../src/components/Unlimited";
 import SmallBoxes from "../src/components/SmallBoxes";
 import TextImg from "../src/components/TextImg";
+import Swiper from "../src/components/Swiper";
+import { FaCheck } from "react-icons/fa";
 // import { CircularProgressbar } from 'react-circular-progressbar';
 
 
@@ -25,81 +27,54 @@ export default function Home(props) {
   return (
     <div>
        
-     
-      <section>
-        <Navbar />
-      </section>
+     <section>
+       <Navbar />
+     </section>
 
-      <section>
-        <div className={"p-11"}>
-          <Card className={cn("border-t-4 border-blue-300")}>
-            
-             </Card>
-        </div>
-      </section>
+     <section>
+       <div className = {cn("bg-white container mx-auto")}>
+       <div className = {cn("md:grid grid-cols-2 gap-4 sm:pb-8 ")}>
+          <div className = {cn("col-span-1 pt-12")}>
+            <FastestSpeed />
+          </div>
+          <div className = {cn("col-span-1 pt-12 sm:pt-16")}>
+           <QuestionAnswer />
+          </div>
+       </div>
+       </div>
+     </section>
 
-      <section>
-        <div className = {cn("p-2")}>
-          <ResumeCard className={cn("border-blue-300")}></ResumeCard>
-        </div>
-      </section>
+     <section className  ={cn("sm:pb-8 md:pb-0 lg:pb-0")}>
+       <InternetProvider />
+     </section>
 
-      <section>
-      <JobOpening />
-      </section>
-
-      <section>
-        <Box />
-      </section>
-
-      <section>
-        <IntSelection />
-      </section>
-
-      <section>
-      {/* <CircularProgressbar value={percentage} text={`${percentage}%`} />; */}
-      <Router />
-      </section>
-
-      <section>
-        <RightPlan />
-      </section>
-
-      <section>
-        <QuestionAnswer />
-      </section>
-
-      <section>
-        <FiberConnection />
-      </section>
-
-      <section>
-        <AddressBar />
-      </section>
-
-      <section>
-        <FastestSpeed />
-      </section>
-
-      <section>
-        <InternetProvider />
-      </section>
-
-      <section>
-        <Unlimited  />
-      </section>
-
-      <section>
-        <SmallBoxes />
-      </section>
-
-      <section>
-        <TextImg />
-      </section>
-
-      <section>
-        <Footer />
-      </section>
+     <section>
+       <div className = {cn("md:grid grid-cols-2")}>
+         <div className = {cn("col-span-1")}>
+       <div className = {cn("couch w-full")}></div>
+       </div>
+       <div className = {cn("col-span-1 p-11")}>
+       <div className = {cn("container mx-auto")}>
+         <p className = {cn("text-blue-800 text-sm")}>Unlimited possible</p>
+         <p className = {cn("font-bold text-2xl text-blue-800")}>The best TV experience powered by NetFx internet</p>
+         <p className = {cn("text-sm text-gray-500")}>we've streamlined our plans to give you the fastest internet available at your address for one low monthly price.</p>
+         <div>
+           <FaCheck className = {cn("inline")}/>
+           <p className = {cn("text-gray-500 text-sm inline")}>The speed you need at a price you'll love-with plans up to 100Mbps*</p>
+         </div>
+         <div className = {cn("pt-3")}>
+           <FaCheck className = {cn("inline")}/>
+           <p className = {cn("text-gray-500 text-sm inline")}>The bandwidth to power multiple devices at once</p>
+         </div>
+         <div className = {cn("pt-3")}>
+           <FaCheck className = {cn("inline")}/>
+           <p className = {cn("text-gray-500 text-sm inline")}>Seamless streaming,working, and gaming</p>
+         </div>
+       </div>
+       </div>
+       </div>
+     </section>
+      
     </div>
   );
 }
