61fad85e7cd0d8590a3135f3c6c61c28b1adf524
Greg <greg_markarian@hotmail.com>
2021-09-24 16:11:56 +0300

    swiper including phone images prices

---
+++ src/components/Swiper.js
@@ -0,0 +1,45 @@
+import React from "react";
+import CardSwipe from "./CardSwipe";
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import Card from "./Card";
+
+export default () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={20}
+      slidesPerView={4}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+    >
+      <SwiperSlide>
+        <CardSwipe />
+      </SwiperSlide>
+      <SwiperSlide>
+        <CardSwipe />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <CardSwipe />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        <CardSwipe />{" "}
+      </SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+    </Swiper>
+  );
+};
