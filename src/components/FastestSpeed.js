41f08804c582217fed2b7d942a2e1949aa61d0d9
Greg <greg_markarian@hotmail.com>
2021-09-24 16:11:31 +0300

    component including internet prices

--- src/components/FastestSpeed.js
+++ src/components/FastestSpeed.js
@@ -7,8 +7,10 @@ function FastestSpeed() {
             <div>
                 <p className = {cn("text-sm text-blue-700")}>unlimited possible</p>
                 <p className = {cn("font-bold")}>Get your fastest speed</p>
-                <div className = {cn("earth relative")}></div>
-                <button className = {cn("text-white text-sm bg-blue-800 p-2 rounded absolute left-20")}>check coverage map</button>
+                <div className = {cn("earth  w-full")}></div>
+                <div className = "flex justify-center items-center">
+                <button className = {cn("text-white text-sm bg-blue-800 p-2 rounded  ")}>check coverage map</button>
+                </div>
             </div>
         </div>
     )
