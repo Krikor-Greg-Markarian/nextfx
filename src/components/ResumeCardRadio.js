29978d0072c4a04d9f9ab90fa8797c3740e768d5
Greg <greg_markarian@hotmail.com>
2021-09-24 16:13:20 +0300

    resume card info radio section

---
+++ src/components/ResumeCardRadio.js
@@ -0,0 +1,86 @@
+import React from "react";
+import styles from "../../styles/Card.module.css";
+import cn from "classnames";
+
+const CardResume = (props) => {
+  const renderMostPopular = () => {
+    if (props.isMostPopular) {
+      return <div>hi its most popular</div>;
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div
+      className={cn("p-3 rounded-lg", styles.mainContainer, props.className)}
+    >
+      {renderMostPopular()}
+      {props.children}
+      <p className={cn("font-bold pb-4")}>Request a call back</p>
+      <p className = {cn("text-sm text-gray-500")}>Were are commited to providing excellent service and creating a stress-free experience for our customers.</p>
+      <div className={cn("inline ")}>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="First Name"
+          className={cn(" border-2 p-1")}
+        />
+      </div>
+      <div className={cn("inline pl-1")}>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Last Name"
+          className={cn(" border-2 p-1")}
+        />
+      </div>
+      <div className={cn("pt-2")}>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Job You're Applying For:"
+          className={cn("w-full border-2  p-1")}
+        />
+        <div className={cn("pt-2")}>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Phone number"
+            className={cn("w-full border-2 p-1 ")}
+          />
+        </div>
+        <div className={cn("pt-2")}>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Business email"
+            className={cn("w-full border-2 p-1")}
+          />
+        </div>
+       <p className = {cn("text-sm pt-3")}>How do you prefer to be contracted? (Optional)</p>
+       <div className = {cn("inline")}> 
+       <input type="radio" name="" id="" />
+       <label htmlFor=""> Email</label>
+       </div>
+       <div className = {cn("pl-20 inline")}>
+       <input type="radio" name="" id="" />
+       <label htmlFor=""> Phone</label>
+       </div>
+        
+      </div>
+      <div className={cn("pt-4 pb-2")}>
+        <button className={cn("bg-blue-800 text-white text-sm p-2 w-full")}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CardResume;
