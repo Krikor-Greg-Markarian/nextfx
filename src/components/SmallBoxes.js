e9b6b3c449437a66ec719fbf42a590ac50aa2918
Greg <greg_markarian@hotmail.com>
2021-09-24 16:13:43 +0300

    small box component

--- src/components/SmallBoxes.js
+++ src/components/SmallBoxes.js
@@ -5,11 +5,11 @@ function SmallBoxes() {
   return (
     <div className={cn("w-3/12 ")}>
       <div className={cn("bg-gray-200 border-r-2 border-gray-800 p-3")}>
-        <div class="flex ">
-          <div class="flex-shrink flex justify-center items-center">
+        <div classname="flex ">
+          <div classname="flex-shrink flex justify-center items-center">
             <div className={cn("small rounded-full")}></div>
           </div>
-          <div class="flex-shrink ">
+          <div classname="flex-shrink ">
             <p className={cn("font-bold text-lg pl-2")}>370+</p>
             <p className={cn("pl-2")}>Available Channels</p>
           </div>
