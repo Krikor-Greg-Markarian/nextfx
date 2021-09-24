212d18a629c5e30c3ce781e67be0f46ea211bbfe
Greg <greg_markarian@hotmail.com>
2021-09-24 16:13:51 +0300

    unlimited component

--- src/components/Unlimited.js
+++ src/components/Unlimited.js
@@ -4,7 +4,7 @@ import cn from "classnames";
 function Unlimited() {
   return (
     <div>
-      <div className={cn("bg-white p-2 w-2/12 border-8")}>
+      <div className={cn("bg-white")}>
         <p className={cn("font-bold")}>Wifi internet</p>
         <p className={cn("text-sm text-gray-500")}>
           High-speed internet access that is always on &amp; fast.
