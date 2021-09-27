a8ca0baa6ca9841a0c3955ec141098bf44d72cc5
Greg <greg_markarian@hotmail.com>
2021-09-27 10:52:58 +0300

    FIX: width of the boxes

--- src/components/AddressBar.js
+++ src/components/AddressBar.js
@@ -7,41 +7,42 @@ function AddressBar() {
       <div className={cn("firebar")}>
         <div className={cn("container mx-auto")}>
           <div className={cn("pt-4")}>
-            <p className={cn("text-white")}>
+            <p className={cn("text-white text-center")}>
               Enter your address to find deals in your area:
             </p>
-            <div className="flex">
-              <div className="flex-grow pl-2">
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Street address"
-                  className={cn("p-2")}
-                />
-              </div>
-              <div className="flex-shrink pl-1">
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Apt/Unite"
-                  className={cn("p-2")}
-                />
-              </div>
-              <div className="flex-shrink pl-1">
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Enter zipcoide"
-                  className={cn("p-2")}
-                />
-              </div>
-              <div className="flex-grow pl-1">
-                <div className={cn("flex justify-center ritems-center")}>
-                  <button className={cn("bg-blue-600 text-sm px-18")}>
-                    Check Availability
+
+            <div className="flex justify-center items-center">
+              <div className={cn("grid grid-cols-4 gap-2 w-9/12")}>
+                <div className={cn("col-span-1")}>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Street address"
+                    className={cn("p-1 w-full")}
+                  />
+                </div>
+                <div className={cn("col-span-1")}>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Apt/unit"
+                    className={cn("p-1 w-full")}
+                  />
+                </div>
+                <div className={cn("col-span-1")}>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Enter zipcode"
+                    className={cn("p-1 w-full")}
+                  />
+                </div>
+                <div className={cn("col-span-1")}>
+                  <button className={cn("bg-blue-700 text-sm p-2")}>
+                    Check Availibility
                   </button>
                 </div>
               </div>
