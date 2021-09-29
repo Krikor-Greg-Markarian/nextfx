f3ae04ff8d11f7994ca7eb13fd83a0a9bd63c19b
Greg <greg_markarian@hotmail.com>
2021-09-29 15:50:36 +0300

    FIX: format document

--- src/components/BlogsClassicitem.js
+++ src/components/BlogsClassicitem.js
@@ -7,16 +7,16 @@ function Box(props) {
       <div className={cn("instrument rounded w-full")}></div>
       <p className={cn("text-gray-500 text-sm pt-2 mb-0")}>
         {props.date}{" "}
-        <span className={cn("text-pink-600 text-sm font-bold")}>{props.name}</span>
+        <span className={cn("text-pink-600 text-sm font-bold")}>
+          {props.name}
+        </span>
       </p>
-      <p className={cn("text-lg text-blue-900 font-bold")}>
-       {props.title}
-      </p>
-      <p className={cn("text-sm text-gray-500")}>{props.description}
-        
-      </p>
+      <p className={cn("text-lg text-blue-900 font-bold")}>{props.title}</p>
+      <p className={cn("text-sm text-gray-500")}>{props.description}</p>
       <li className={cn(" text-sm list-none")}>
-        <a href="#" className = {cn("text-blue-900 no-underline font-bold")}>{props.tag}</a>
+        <a href="#" className={cn("text-blue-900 no-underline font-bold")}>
+          {props.tag}
+        </a>
       </li>
     </div>
   );
