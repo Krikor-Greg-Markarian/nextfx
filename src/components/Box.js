a475683cd89e056c4ece6a78ebac1f7bfc5485dd
Greg <greg_markarian@hotmail.com>
2021-09-29 15:50:40 +0300

    FIX: format document

--- src/components/Box.js
+++ src/components/Box.js
@@ -7,7 +7,9 @@ function Box(props) {
       <div className={cn("instrument rounded w-full")}></div>
       <p className={cn("text-gray-500 text-sm pt-2 mb-0")}>
         May 11,2020 / By{" "}
-        <span className={cn("text-pink-600 text-sm font-bold")}>Emma Waston</span>
+        <span className={cn("text-pink-600 text-sm font-bold")}>
+          Emma Waston
+        </span>
       </p>
       <p className={cn("text-lg text-blue-900 font-bold")}>
         What is the future of enterprise internet ?
@@ -19,7 +21,9 @@ function Box(props) {
         itaque nemo?
       </p>
       <li className={cn(" text-sm list-none")}>
-        <a href="#" className = {cn("text-blue-900 no-underline font-bold")}>Read More</a>
+        <a href="#" className={cn("text-blue-900 no-underline font-bold")}>
+          Read More
+        </a>
       </li>
     </div>
   );
