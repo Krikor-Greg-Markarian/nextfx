394d9fa5a5846acb627f8d5bd97954637a4572d3
Greg <greg_markarian@hotmail.com>
2021-09-29 16:05:00 +0300

    ADD: props title descrition

--- src/components/FastestSpeed.js
+++ src/components/FastestSpeed.js
@@ -1,20 +1,18 @@
 import React from "react";
 import cn from "classNames";
 
-function FastestSpeed() {
+function FastestSpeed(props) {
   return (
     <div>
       <div>
-        <p className={cn("text-sm text-blue-700 mb-0")}>unlimited possible</p>
-        <p className={cn("font-bold mt-0")}>Get your fastest speed</p>
+        <p className={cn("text-sm text-blue-700  pt-12 mb-0")}>
+          {props.smalltitle}
+        </p>
+        <p className={cn("font-bold  text-2xl text-blue-900")}>
+          {props.description}
+        </p>
         <div className={cn("earth  w-full")}></div>
-        <div className="flex justify-center items-center">
-          <button
-            className={cn("text-white text-sm bg-blue-800 p-2 rounded  ")}
-          >
-            check coverage map
-          </button>
-        </div>
+        <div className="flex justify-center items-center"></div>
       </div>
     </div>
   );
