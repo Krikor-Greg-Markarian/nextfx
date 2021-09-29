85242aa36199af01a2a6b2983e85e45371b47076
Greg <greg_markarian@hotmail.com>
2021-09-29 17:08:04 +0300

    ADD: Shadow card shape component

---
+++ src/components/CardShadowRec.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/Card.module.css";
+import cn from "classnames";
+import "react-circular-progressbar/dist/styles.css";
+
+function CardShadow(props) {
+  return (
+    <div className={cn("p-4", styles.mainContainer)}>{props.children}</div>
+  );
+}
+
+export default CardShadow;
