c2e9d211f014bb82be0f4d6bab1558697a6194a4
Greg <greg_markarian@hotmail.com>
2021-09-29 15:47:04 +0300

    ADD: padding

--- src/components/SmallImagText.js
+++ src/components/SmallImagText.js
@@ -1,29 +1,15 @@
 import React from "react";
 import cn from "classnames";
 
-const Info = (props) => {
+function SmallImagText(props) {
   return (
     <div>
       <div className={cn("small rounded-full")}> </div>
-      <p className={cn("text-blue-900 pt-3 font-bold")}>
-        Network &amp; Connectivity{props.title}
-      </p>
+      <p className={cn("text-blue-900 pt-3 font-bold")}>&amp; {props.title}</p>
       <p className={cn("text-sm text-gray-500")}>{props.subtitle}</p>
       <a href="" className={cn("text-blue-900 font-bold no-underline text-sm")}>
         {props.subsubtitle}
       </a>
-    </div>
-  );
-};
-
-function SmallImagText() {
-  return (
-    <div>
-      <Info
-        subtitle=" Solutions to optimize performance and deliver the bandwith and
-        reliability needed to support IT transformation."
-        subsubtitle="Learn more"
-      />
     </div>
   );
 }
