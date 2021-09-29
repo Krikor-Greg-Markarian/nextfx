e0db00b25e59303fc17223b3b76e516148dce33b
Greg <greg_markarian@hotmail.com>
2021-09-29 17:08:20 +0300

    ADD: props title description

---
+++ src/components/Experienceitem.js
@@ -0,0 +1,15 @@
+import React from "react";
+import cn from "classnames";
+import CardShadowRec from "./CardShadowRec";
+
+function Experienceitem(props) {
+  return (
+    <CardShadowRec>
+      <div className={cn("small rounded-full")}></div>
+      <p className={cn("font-bold mb-0 pt-3")}>{props.title}</p>
+      <p className={cn("text-gray-500 text-sm pt-2")}>{props.description}</p>
+    </CardShadowRec>
+  );
+}
+
+export default Experienceitem;
