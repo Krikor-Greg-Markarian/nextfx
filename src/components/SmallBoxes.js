ac272cc1ecf87152db61495ee58689be805f9da0
Greg <greg_markarian@hotmail.com>
2021-09-29 15:51:09 +0300

    FIX: format document

--- src/components/SmallBoxes.js
+++ src/components/SmallBoxes.js
@@ -4,16 +4,16 @@ import cn from "classNames";
 function SmallBoxes() {
   return (
     <div className={cn("")}>
-      <div className = {cn("pt-1 pl-10")}>
-      <div class="flex bg-gray-200 p-2 border-r-4 border-gray-500 ">
-        <div class="flex-shrink ">
-          <div className={cn("small rounded-full")}></div>
+      <div className={cn("pt-1 pl-10")}>
+        <div class="flex bg-gray-200 p-2 border-r-4 border-gray-500 ">
+          <div class="flex-shrink ">
+            <div className={cn("small rounded-full")}></div>
+          </div>
+          <div class="flex-shrink ">
+            <p className={cn("font-bold")}>370+</p>
+            <p className={cn("text-gray-500 text-sm")}>Available Channels</p>
+          </div>
         </div>
-        <div class="flex-shrink ">
-          <p className={cn("font-bold")}>370+</p>
-          <p className={cn("text-gray-500 text-sm")}>Available Channels</p>
-        </div>
-      </div>
       </div>
     </div>
   );
