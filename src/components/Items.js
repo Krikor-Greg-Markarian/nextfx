9700e4e4902e2e2ba75d26b881be4b6442d85738
Greg <greg_markarian@hotmail.com>
2021-09-29 15:46:17 +0300

    ADD: mapping process

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,31 +1,15 @@
 import React from "react";
-import cn from "classnames";
 import { FaCheck } from "react-icons/fa";
 
-const Item = (props) => {
+const Items = (props) => {
   return (
-    <div>
-      <div class="flex ">
-        <div class="flex-shrink pt-2">
-          <FaCheck />
-        </div>
-        <div class="flex-shrink text-sm text-gray-500 pl-3 pt-1">
-          {props.text}
-        </div>
+    <div class="flex ">
+      <div class="flex-shrink pt-2">{props.icon ?? <FaCheck />}</div>
+      <div class="flex-shrink text-sm text-gray-500 pl-3 pt-1">
+        {props.text}
       </div>
     </div>
   );
 };
-
-function Items(props) {
-  return (
-    <div>
-      <Item text="Free Google Voice Remote and DVR included" />
-      <Item text="60+ HD Channels" />
-      <Item text="Local Channels" />
-      <Item text="190 Channels" />
-    </div>
-  );
-}
 
 export default Items;
