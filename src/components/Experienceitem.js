0ab353eea59fe6a7d300dda100fc14fa03cb6dc4
Greg <greg_markarian@hotmail.com>
2021-09-30 17:05:18 +0300

    ADD: const background color etxt color pros were added

--- src/components/Experienceitem.js
+++ src/components/Experienceitem.js
@@ -3,11 +3,19 @@ import cn from "classnames";
 import CardShadowRec from "./CardShadowRec";
 
 function Experienceitem(props) {
+
+const styleClasses = {
+  backgroundcolor:props.backgroundcolor ?? "bg-white",
+  titlecolor:props.titlecolor ?? "text-black",
+  description:props.description ?? "text-black",
+}
+
+
   return (
     <CardShadowRec>
       <div className={cn("small rounded-full")}></div>
-      <p className={cn("font-bold mb-0 pt-3")}>{props.title}</p>
-      <p className={cn("text-gray-500 text-sm pt-2")}>{props.description}</p>
+      <p className={cn("font-bold mb-0 pt-3", styleClasses.titlecolor)}>{props.title}</p>
+      <p className={cn("text-gray-500 text-sm pt-2", styleClasses.description)}>{props.description}</p>
     </CardShadowRec>
   );
 }
