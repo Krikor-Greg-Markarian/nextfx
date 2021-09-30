f87b89467fcd79bf223df52d4ae3df2929c21aea
Greg <greg_markarian@hotmail.com>
2021-09-30 11:24:44 +0300

    FIX: format documnet

--- src/components/ClientReview.js
+++ src/components/ClientReview.js
@@ -20,42 +20,64 @@ function ClientReview(props) {
           </p>
         </div>
       </div>
-   <div className= {cn("grid grid-cols-4 pt-6")}>
-     <div className = {cn("col-span-1")}>
-     <div class="flex">
-  <div class="flex-shrink">
-    <div className = {cn("small rounded-lg")}></div>
-  </div>
-  <div class="flex-shrink pl-1">
-   <p className = {cn("text-blue-900 text-sm font-bold pl-1")}>Global News</p>
-  </div>
-  
-</div>
-     </div>
+      <div className={cn("grid grid-cols-4 pt-6")}>
+        <div className={cn("col-span-1")}>
+          <div class="flex">
+            <div class="flex-shrink">
+              <div className={cn("small rounded-lg")}></div>
+            </div>
+            <div class="flex-shrink pl-1">
+              <p className={cn("text-blue-900 text-sm font-bold pl-1")}>
+                Global News
+              </p>
+            </div>
+          </div>
+        </div>
 
-     <div className = {cn("col-span-1 pl-6 relative")}>
-       
-    <div className = {cn("bg-blue-300 w-14 text-sm text-blue-900 font-bold relative p-1")}>NEWS  <div className = {cn("bg-blue-900 w-6 text-white text-xs absolute left-11 top-5 text-center p-1")}>TV</div> </div>
-    
-    </div>
-     
+        <div className={cn("col-span-1 pl-6 relative")}>
+          <div
+            className={cn(
+              "bg-blue-300 w-14 text-sm text-blue-900 font-bold relative p-1"
+            )}
+          >
+            NEWS{" "}
+            <div
+              className={cn(
+                "bg-blue-900 w-6 text-white text-xs absolute left-11 top-5 text-center p-1"
+              )}
+            >
+              TV
+            </div>{" "}
+          </div>
+        </div>
 
-     <div className = {cn("col-span-1")}>
-     <div class="flex">
- 
-  <div class="flex-shrink pl-6">
-   <p className = {cn("text-blue-900 text-sm font-bold mb-0")}>N E W S</p>
-   <p className = {cn("text-xs text-blue-900 font-bold")}>BUSINESS</p>
-  </div>
- 
-  
-</div>
-     </div>
-     <div className = {cn("col-span-1 pl-6")}>
-     <div className = {cn("bg-blue-300 rounded-full w-11 h-11 relative font-bold p-2 text-center text-white")}>One <div className = {cn("bg-blue-800 rounded-full w-6 h-6 absolute text-xs -top-1 left-9 p-1 text-white text-center")}>TV</div></div>
-     </div>
-   </div>
-
+        <div className={cn("col-span-1")}>
+          <div class="flex">
+            <div class="flex-shrink pl-6">
+              <p className={cn("text-blue-900 text-sm font-bold mb-0")}>
+                N E W S
+              </p>
+              <p className={cn("text-xs text-blue-900 font-bold")}>BUSINESS</p>
+            </div>
+          </div>
+        </div>
+        <div className={cn("col-span-1 pl-6")}>
+          <div
+            className={cn(
+              "bg-blue-300 rounded-full w-11 h-11 relative font-bold p-2 text-center text-white"
+            )}
+          >
+            One{" "}
+            <div
+              className={cn(
+                "bg-blue-800 rounded-full w-6 h-6 absolute text-xs -top-1 left-9 p-1 text-white text-center"
+              )}
+            >
+              TV
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
