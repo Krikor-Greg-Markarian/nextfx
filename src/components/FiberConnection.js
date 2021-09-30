1b7fbb3caf389f8746b525e060ab8b26d0c8da8e
Greg <greg_markarian@hotmail.com>
2021-09-30 15:22:43 +0300

    FIX: fixes the free color

--- src/components/FiberConnection.js
+++ src/components/FiberConnection.js
@@ -9,7 +9,7 @@ function FiberConnection() {
           <div className="grid grid-cols-2">
             <div className="col-span-1">
               <div className={cn("pt-12")}>
-                <p className={cn("text-blue-800 font-bold")}>
+                <p className={cn("text-blue-300 font-bold")}>
                   FREE{" "}
                   <span className={cn("text-white font-bold")}>
                     {" "}
