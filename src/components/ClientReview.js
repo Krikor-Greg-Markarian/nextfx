7c4c40b43e2868339b1c8c5ad1c96c1a2a9f96c5
Greg <greg_markarian@hotmail.com>
2021-09-30 09:46:16 +0300

    FIX: Added props title name description

--- src/components/ClientReview.js
+++ src/components/ClientReview.js
@@ -4,20 +4,19 @@ import cn from "classnames";
 function ClientReview(props) {
   return (
     <div className={cn("py-28")}>
-      <p className={cn("text-sm text-blue-700 mb-0")}>Testimonials</p>
-      <p className={cn("font-bold text-2xl")}>Our client love us!</p>
+      <p className={cn("text-sm text-blue-700 mb-0")}>{props.smalltitle}</p>
+      <p className={cn("font-bold text-2xl text-blue-900")}>{props.title}</p>
       <p className={cn("text-sm text-gray-500 pt-2 pb-2")}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim
-        eaque ipsum qui voluptates, consectetur tempore quas odit!
+        {props.description}
       </p>
       <div class="flex">
         <div class="flex-shrink">
           <div className={cn("small rounded-full")}></div>
         </div>
         <div class="flex-shrink pl-2">
-          <p className={cn("font-bold text-sm mb-0")}>Alex Rovia</p>
+          <p className={cn("font-bold text-sm mb-0")}>{props.name}</p>
           <p className={cn("text-gray-500 text-sm inline")}>
-            Los Angeles,California <span className={cn("pl-5")}>*****</span>{" "}
+            {props.location} <span className={cn("pl-5")}>*****</span>{" "}
           </p>
         </div>
       </div>
