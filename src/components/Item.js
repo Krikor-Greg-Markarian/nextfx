a09779810310c9d9a7b9a8e51f9bf50687638881
Greg <greg_markarian@hotmail.com>
2021-09-30 13:08:50 +0300

    ADD: changed the check color from black to blue

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,9 @@ import { FaCheck } from "react-icons/fa";
 const Item = (props) => {
   return (
     <div class="flex ">
-      <div class="flex-shrink pt-2">{props.icon ?? <FaCheck />}</div>
+      <div class="flex-shrink pt-2 text-blue-300">
+        {props.icon ?? <FaCheck />}
+      </div>
       <div class="flex-shrink text-sm text-gray-500 pl-3 pt-2">
         {props.text}
       </div>
