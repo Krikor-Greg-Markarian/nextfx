a88f1e58c93646a0e711a9cfa47e7db3089d4558
Greg <greg_markarian@hotmail.com>
2021-09-30 12:52:51 +0300

    FIX: sizing of the fonts xs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -36,7 +36,7 @@ function Footer() {
             <div className={cn("grid grid-cols-6 pt-4")}>
               <div className={cn("col-span-2  ")}>
                 <p className={cn("font-bold text-blue-800")}>About company</p>
-                <p className={cn("text-sm pt-4 text-gray-500 ")}>
+                <p className={cn("text-xs pt-4 text-gray-500 ")}>
                   we're developing the ultimate communications network to power
                   high-quality, secure,affordable, fast connections.
                 </p>
@@ -51,9 +51,8 @@ function Footer() {
                 <p className={cn("font-bold text-blue-800")}>Contacts</p>
                 <div classname="flex pt-4">
                   <div classname="flex-shrink ">
-                    <FaLocationArrow className={cn("inline text-blue-800")} />
-                    <p className={cn("text-sm inline text-gray-500")}>
-                      {" "}
+                    <FaLocationArrow className={cn("inline text-blue-800 ")} />
+                    <p className={cn("text-xs inline text-gray-500 pl-1")}>
                       5002 Examples Street Los Angeles, CA 90034
                     </p>
                   </div>
@@ -61,8 +60,7 @@ function Footer() {
                 <div classname="flex pt-4">
                   <div classname="flex-shrink ">
                     <FaPhone className={cn("inline text-blue-800")} />
-                    <p className={cn("text-sm inline text-gray-500")}>
-                      {" "}
+                    <p className={cn("text-xs inline text-gray-500 pl-1")}>
                       example@company.com
                     </p>
                   </div>
@@ -70,8 +68,7 @@ function Footer() {
                 <div classname="flex pt-4">
                   <div classname="flex-shrink ">
                     <FaMailBulk className={cn("inline text-blue-800")} />
-                    <p className={cn("text-sm inline text-gray-500")}>
-                      {" "}
+                    <p className={cn("text-xs inline text-gray-500 pl-1")}>
                       012-2453-6353
                     </p>
                   </div>
@@ -80,27 +77,27 @@ function Footer() {
 
               <div className={cn("pl-8")}>
                 <p className={cn("font-bold text-blue-800")}>Navigation</p>
-                <li className={cn("text-sm list-none")}>
+                <li className={cn("text-xs list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Home
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Services
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     About us
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Testimonials
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Blogs
                   </a>
@@ -108,27 +105,27 @@ function Footer() {
               </div>
               <div className={cn("pl-7")}>
                 <p className={cn("font-bold text-blue-800")}>Quick links</p>
-                <li className={cn("text-sm list-none")}>
+                <li className={cn("text-xs list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Packages
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Connection
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Service Plus
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Privacy Policy
                   </a>
                 </li>
-                <li className={cn("text-sm pt-2 list-none")}>
+                <li className={cn("text-xs pt-2 list-none")}>
                   <a className={cn("no-underline text-gray-500")} href="#">
                     Coverage Maps
                   </a>
@@ -139,14 +136,20 @@ function Footer() {
               <div className={cn("pt-4 pb-1")}>
                 <p
                   className={cn(
-                    "border-t-2 border-gray-300 pt-3 text-sm text-gray-500 "
+                    "border-t-2 border-gray-300 pt-3 text-xs text-gray-500 "
                   )}
                 >
                   NetFx <span className={cn("pl-4")}>|</span>{" "}
-                  <span className={cn("pl-4")}>&copy; 2020JWSTHEMES </span>{" "}
-                  <span className={cn("float-right pl-4")}>Sitemap</span>{" "}
-                  <span className={cn("float-right pl-4")}>Privacy</span>{" "}
-                  <span className={cn("float-right")}>Legal</span>{" "}
+                  <span className={cn("pl-4")}>
+                    Copyright &copy; 2020JWSTHEMES{" "}
+                  </span>
+                  <span className={cn("float-right pl-4 text-xs")}>
+                    Sitemap
+                  </span>
+                  <span className={cn("float-right pl-4 text-xs")}>
+                    Privacy
+                  </span>
+                  <span className={cn("float-right text-xs")}>Legal</span>
                 </p>
               </div>
             </div>
