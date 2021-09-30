8d6464448c2aecdf36bddbf4ccb9e0b4070bc136
Greg <greg_markarian@hotmail.com>
2021-09-30 15:24:05 +0300

    FIX: props for different buttons background colors

--- src/components/RightPlanitem.js
+++ src/components/RightPlanitem.js
@@ -1,20 +1,33 @@
 import React from "react";
 import cn from "classnames";
-import { FaCheck } from "react-icons/fa";
 import Item from "./Item";
 import Button from "./Button";
+import { prop } from "dom7";
 
 function RightPlanitem(props) {
+  const styleClasses = {
+    planBackgroundColor: props.planBackgroundColor ?? "bg-white",
+    smallTextColor: "text-gray-500",
+    titleColor: "text-blue-900",
+    priceColor: "text-blue-800",
+    buttonBackgroundColor: "bg-gray-200",
+    buttonTextColor: "text-blue-600",
+  };
+
   return (
-    <div className={cn("bg-blue-100 rounded")}>
+    <div className={cn("rounded shadow-md", styleClasses.planBackgroundColor)}>
       <div className={cn("p-3")}>
-        <p className={cn("text-sm font-bold")}>{props.smalltitle}</p>
-        <p className={cn("font-bold text-lg mb-0 text-blue-900")}>
+        <p className={cn("text-sm font-bold", styleClasses.smallTextColor)}>
+          {props.smalltitle}
+        </p>
+        <p className={cn("font-bold text-lg mb-0", styleClasses.titleColor)}>
           {props.title}
         </p>
-        <p className={cn("font-bold text-blue-800 text-lg")}>
+        <p className={cn("font-bold text-lg", styleClasses.price)}>
           {props.price}{" "}
-          <sub className={cn("text-sm text-gray-500")}>{props.month}</sub>{" "}
+          <sub className={cn("text-sm ", styleClasses.smallTextColor)}>
+            {props.month}
+          </sub>{" "}
         </p>
 
         {props.item.map((item, idx) => (
@@ -23,9 +36,11 @@ function RightPlanitem(props) {
         <div className={cn("pt-3 flex justify-center items-center")}>
           <Button
             className={cn(
-              "bg-blue-800 text-white text-center p-2 rounded w-9/12"
+              "text-center p-2 rounded w-9/12",
+              styleClasses.buttonBackgroundColor,
+              styleClasses.buttonTextColor
             )}
-            text="Order now"
+            text={props.text}
           />
         </div>
       </div>
