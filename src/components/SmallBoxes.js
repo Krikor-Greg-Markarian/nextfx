eba3ca050184fd90161f000f691e5b32a109cab1
Greg <greg_markarian@hotmail.com>
2021-09-30 09:29:27 +0300

    ADD: props changed text

--- src/components/SmallBoxes.js
+++ src/components/SmallBoxes.js
@@ -3,16 +3,14 @@ import cn from "classNames";
 
 function SmallBoxes(props) {
   return (
-    <div className={cn("")}>
-      <div className={cn("pt-1 pl-10")}>
-        <div class="flex bg-gray-200 p-2 border-r-4 border-gray-500 ">
-          <div class="flex-shrink ">
-            <div className={cn("small rounded-full")}></div>
-          </div>
-          <div class="flex-shrink pl-3">
-            <p className={cn("font-bold mb-0 text-2xl")}>{props.channel}</p>
-            <p className={cn("text-gray-500 text-xs")}>{props.available}</p>
-          </div>
+    <div className={cn("pt-1 pl-10")}>
+      <div class="flex bg-gray-200 p-2 border-r-4 border-gray-500">
+        <div class="flex-shrink">
+          <div className={cn("small rounded-full")}></div>
+        </div>
+        <div class="flex-shrink pl-3">
+          <p className={cn("font-bold mb-0 text-2xl")}>{props.channel}</p>
+          <p className={cn("text-gray-500 text-xs")}>{props.available}</p>
         </div>
       </div>
     </div>
