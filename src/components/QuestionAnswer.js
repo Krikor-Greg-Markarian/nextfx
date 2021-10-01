f420948162d49ff625da4660821186f6b81393b3
Greg <greg_markarian@hotmail.com>
2021-10-01 13:20:14 +0300

    DEL: deleted classname section unused

--- src/components/QuestionAnswer.js
+++ src/components/QuestionAnswer.js
@@ -4,7 +4,7 @@ import Accordion from "react-bootstrap/Accordion";
 
 function QuestionAnswer() {
   return (
-    <div className={cn("")}>
+    <div>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
           <Accordion.Header>Why NetFx home internet service?</Accordion.Header>
