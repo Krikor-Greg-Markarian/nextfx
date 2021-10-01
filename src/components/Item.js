e403a48486a8a0eac392373a995a6df762ff7881
Greg <greg_markarian@hotmail.com>
2021-10-01 12:02:15 +0300

    ADD: props.children

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ const Item = (props) => {
   return (
     <div className="flex ">
       <div className="flex-shrink pt-2 text-blue-300">
-        {props.icon ?? <FaCheck />}
+        {props.icon ?? <FaCheck />}{props.children}
       </div>
       <div className="flex-shrink text-sm text-gray-400 pl-3 pt-2">
         {props.text}
