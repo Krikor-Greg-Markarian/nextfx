59c692a481f94085d315cc015b27adb3711fa2f4
Greg <greg_markarian@hotmail.com>
2021-10-01 13:19:53 +0300

    FIX: sm md response fix

--- src/components/Experienceitem.js
+++ src/components/Experienceitem.js
@@ -4,7 +4,7 @@ import CardShadowRec from "./CardShadowRec";
 
 function Experienceitem(props) {
   return (
-    <CardShadowRec>
+    <CardShadowRec className = {cn("sm:mt-1 md:mt-1")}>
       <div className={cn("small rounded-full")}></div>
       <p className={cn("font-bold mb-0 pt-3")}>{props.title}</p>
       <p className={cn("text-gray-500 text-sm pt-2")}>{props.description}</p>
