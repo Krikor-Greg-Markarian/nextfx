f4fe5eb15eae6b5814745ed4b052e01ca3e42800
Greg <greg_markarian@hotmail.com>
2021-10-01 16:24:54 +0300

    FIX: added images instead of background image

--- src/components/SmallBoxes.js
+++ src/components/SmallBoxes.js
@@ -6,7 +6,11 @@ function SmallBoxes(props) {
     <div className={cn("pt-1 pl-10")}>
       <div className="flex bg-gray-200 p-2 border-r-4 border-gray-500">
         <div className="flex-shrink">
-          <div className={cn("small rounded-full")}></div>
+          <img
+            className={cn("rounded-full w-12 h-12")}
+            src={props.imageUrl}
+            alt=""
+          />
         </div>
         <div className="flex-shrink pl-3">
           <p className={cn("font-bold mb-0 text-2xl")}>{props.channel}</p>
