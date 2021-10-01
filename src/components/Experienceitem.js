4bbe68d66fd7cf03569741e879e848e5fea2b7dc
Greg <greg_markarian@hotmail.com>
2021-10-01 16:24:39 +0300

    FIX: added images instead of backgournd image

--- src/components/Experienceitem.js
+++ src/components/Experienceitem.js
@@ -4,8 +4,12 @@ import CardShadowRec from "./CardShadowRec";
 
 function Experienceitem(props) {
   return (
-    <CardShadowRec className = {cn("sm:mt-1 md:mt-1")}>
-      <div className={cn("small rounded-full")}></div>
+    <CardShadowRec className={cn("sm:mt-1 md:mt-1")}>
+      <img
+        className={cn("rounded-full w-12 h-12")}
+        src={props.imageUrl}
+        alt=""
+      />
       <p className={cn("font-bold mb-0 pt-3")}>{props.title}</p>
       <p className={cn("text-gray-500 text-sm pt-2")}>{props.description}</p>
     </CardShadowRec>
