d656af1de31f6711eb919c78ae0f14f98f1b122b
Greg <greg_markarian@hotmail.com>
2021-10-01 12:18:25 +0300

    FIX: changed sm text to xs

--- src/components/JobOpening.js
+++ src/components/JobOpening.js
@@ -9,19 +9,19 @@ function JobOpening(props) {
   return (
     <div className={cn("grid grid-cols-4", styleClass.backgroundcolor)}>
       <div className={cn("col-span-1 ")}>
-        <p className={cn("text-blue-800 font-bold text-sm pt-2 text-center")}>
+        <p className={cn("text-blue-800 font-bold text-xs pt-2 text-center")}>
           {props.title}
         </p>
       </div>
       <div className={cn("col-span-1 pt-2")}>
-        <p className={cn("text-gray-500 text-sm text-center")}>{props.city}</p>
+        <p className={cn("text-gray-500 text-xs text-center")}>{props.city}</p>
       </div>
       <div className={cn("col-span-1 pt-2")}>
-        <p className={cn("text-gray-500 text-sm text-center")}>{props.time}</p>
+        <p className={cn("text-gray-500 text-xs text-center")}>{props.time}</p>
       </div>
 
       <div className={cn("flex justify-center items-center")}>
-        <button className={cn("text-sm p-2 bg-blue-400 w-8/12 rounded")}>
+        <button className={cn("text-xs p-2 bg-blue-400 w-8/12 rounded")}>
           {props.button}
         </button>
       </div>
